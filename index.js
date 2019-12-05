4bad983002254e2136a6d364b112c5d00d20f394
GeekEast <sx.ba2017@gmail.com>
2019-12-05 15:40:16 +1030

    create project

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const _ = require('lodash');
+
+const reduceBy = (things,key) => _.reduce(things, (map, thing) => {
+    const by = thing[key];
+    (map[by] || (map[by] = [])).push(thing);
+    return map; // update map
+  });
