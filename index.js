2dc20d805f067d9e1d3394173f80a82544157f18
GeekEast <sx.ba2017@gmail.com>
2019-12-05 15:49:04 +1030

    reduceBy()

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const _ = require('lodash');
 
-const reduceBy = (things,key) => _.reduce(things, (map, thing) => {
+export const reduceBy = (things,key) => _.reduce(things, (map, thing) => {
     const by = thing[key];
     (map[by] || (map[by] = [])).push(thing);
     return map; // update map
   },{}); 
 
-var things = [
-    { name: 'foo', input: 'bar' },
-    { name: 'baz', input: 'zle' },
-    { name: 'foo', input: 'ss'},
-];
-
-console.log(reduceBy(things,'name'));
